4bbec9c25593c0482a015478c4885f8a92db6382
Karol-Trixss <KarolBhandari@healthtrixss.com>
2025-03-11 00:34:05 +0545

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gunicorn --workers 4 --worker-class uvicorn.workers.UvicornWorker --timeout 600 --bind 0.0.0.0:8889 app:app
+gunicorn --workers 4 --worker-class uvicorn.workers.UvicornWorker --timeout 600 --bind 0.0.0.0:8000 app:app
