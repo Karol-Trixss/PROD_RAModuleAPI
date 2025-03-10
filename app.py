305faf6afcb2f80e36799bdd30f7bfde958c908d
Karol-Trixss <KarolBhandari@healthtrixss.com>
2025-03-11 00:34:20 +0545

    Update app.py

--- app.py
+++ app.py
@@ -283,5 +283,5 @@ async def health_check():
 
 if __name__ == '__main__':
     import uvicorn
-    port = int(os.getenv("PORT", 8889))
+    port = int(os.getenv("PORT", 8000))
     uvicorn.run(app, host='0.0.0.0', port=port)
